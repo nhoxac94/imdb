382b263dfca9fa3ed747f2697ccef966215919f1
Loi <lehoangloi22041994@gmail.com>
2023-02-23 14:57:00 +0700

    add tailwincss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return <h1 className='text-red-500'>Hello World!</h1>;
 }

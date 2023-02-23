c7e7e72c24413333c852af8a094e968baee9a53d
Loi <lehoangloi22041994@gmail.com>
2023-02-23 16:02:34 +0700

    create header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css';
+import Header from '@/components/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head />
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+
+        {children}
+      </body>
     </html>
   );
 }
